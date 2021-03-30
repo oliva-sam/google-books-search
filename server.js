c3edb7a9069c6ad41e634372b4080287b290ca51
Melody Monarch <oliva.sam.so@gmail.com>
2021-03-30 18:56:46 -0500

    connect mongoose and routes

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+// Dependencies
+
 const express = require("express");
-const path = require("path");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-
+// MiddleWare
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+};
 
 // Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);

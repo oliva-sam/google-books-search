3e00f0e07d5c433bf8697283c24201efd782e200
Melody Monarch <oliva.sam.so@gmail.com>
2021-03-30 21:20:39 -0500

    setup backend api routes

---
+++ routes/api/books.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+router.route("/")
+    .get(booksController.findAll);
+
+module.exports = router;
